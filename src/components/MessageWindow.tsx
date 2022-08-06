82f376192c2ee1bfef27fcbcaa04a708845818e9
Alex Pritchard <alexbeet1234@gmail.com>
2022-08-06 17:26:45 +0100

    created DadMessage

--- src/components/MessageWindow.tsx
+++ src/components/MessageWindow.tsx
@@ -1,14 +1,16 @@
 import GetJokeButton from "./GetJokeButton";
 import UserMessage from "./UserMessage";
+import DadMessage from "./DadMessage";
 
 const MessageWindow = () => {
-  let messageArray: Array<any> = [<UserMessage />];
+  let messageArray: Array<any> = [<UserMessage />, <DadMessage />];
   // useState - set up array that includes type message
   // itterate through array. Check if type messge is user or dad
 
   const getNewJokeHandler = () => {
     // Call dad jokes api to get joke
     // Create a user message asking dad for joke
+    // user message prompt joke
     // Create a dad message with set up
     // Create a user message with response to set up
     // Create a dad message with punchine
