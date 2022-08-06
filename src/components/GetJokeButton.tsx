10e894339e97ba4a5046c29baabdd127a4774c0d
Alex Pritchard <alexbeet1234@gmail.com>
2022-08-06 14:32:55 +0100

    created components folder

---
+++ src/components/GetJokeButton.tsx
@@ -0,0 +1,5 @@
+const GetJokeButton = () => {
+    return <div></div>;
+};
+
+export default GetJokeButton;
---
+++ src/components/MessageWindow.tsx
@@ -0,0 +1,11 @@
+import GetJokeButton from "./GetJokeButton";
+
+const MessageWindow = () => {
+  return (
+    <div>
+      <GetJokeButton />
+    </div>
+  );
+};
+
+export default MessageWindow;
